c9d6d40e1b5fe3b82a93c9ecb1ce9dd03747ca58
Your Name <snyacyesarumuri2@huskers.unl.edu>
2022-08-29 21:26:46 +0000

    Added print line for name

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: NAME: Salem Rumuri Nyacyesa
+ *         EMAIL: snyacyesarumuri2@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World, I am Salem Rumuri Nyacyesa.\n");
+  printf("My Major is Biological Systems Engineering.\n");
+
+  return 0;
+}
